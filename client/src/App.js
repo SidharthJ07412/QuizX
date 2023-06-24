4b673ad0da11ad7cd90d9f294cdad327b6bb226f
Sidharth <sidharth07412@gmail.com>
2023-06-24 15:54:59 +0530

    app.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,25 @@ import SignUp from "./components/SignUp";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import Leaderboard from "./components/Leaderboard";
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Link
+} from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-
-      <Leaderboard />
-
-      <Login />
-      <SignUp/>
-    </div>
+  
+    <Routes>
+          <Route exact path='/' element={< Login />}></Route>
+          <Route exact path='/Signup' element={< SignUp />}></Route>
+          <Route exact path='/Leaderboard' element={< Leaderboard />}></Route>
+   </Routes>
+   </div>
+</Router>
   );
 }
-
 export default App;
