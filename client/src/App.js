4bad795810389bff446a3fe95d85aadc5e0decea
Sidharth <sidharth07412@gmail.com>
2023-07-15 18:17:21 +0530

    kanodia

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import SignUp from "./components/SignUp";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import Leaderboard from "./components/Leaderboard";
+import AddQuestions from "./components/AddQuestions";
+import QuizWindow from "./components/QuizWindow";
 import {
 	BrowserRouter as Router,
 	Routes,
@@ -15,11 +17,14 @@ function App() {
   return (
     <Router>
     <div className="App">
-  
+
     <Routes>
           <Route exact path='/' element={< Login />}></Route>
           <Route exact path='/Signup' element={< SignUp />}></Route>
           <Route exact path='/Leaderboard' element={< Leaderboard />}></Route>
+          <Route exact path='/AddQuestions' element={< AddQuestions />}></Route>
+          <Route exact path='/QuizWindow' element={< QuizWindow />}></Route>
+          
    </Routes>
    </div>
 </Router>
